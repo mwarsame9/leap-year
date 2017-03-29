dda9b845fb95f816ff8ee79b9bb6787d1c9f5138
Mohamed Warsame and Sara Schultz <sara@>
2017-03-29 08:49:20 -0700

    pass first spec

---
+++ README.md
@@ -0,0 +1,6 @@
+| behavior |  input   |  output  |
+|----------|:--------:|:--------:|
+| A year is not a leap year if it is not divisible by 4 | 1993 | false |
+| A year is a leap year if it is divisible by 4 | 2004 | true |
+| A year is not a leap year if it is divisible by 100 | 1900 | false |
+| A year is a leap year if it is divisible by 400 | 2000 | true |
---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+//Business logic:
+var leapYear = function(year) {
+  if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//User interface logic:
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
